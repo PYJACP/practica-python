d83cc5ff0f98028e5cb187224aa448879bbe6134
Soy Sebastian <sebastian1096r@gmail.com>
2026-03-30 09:44:03 -0500

    Mejorando mi codigo

--- programa.py
+++ programa.py
@@ -1,28 +1,33 @@
-import random
+import random,logging
 
 
-def gets_answer(answer_number):
-    if answer_number==1:
-        return 'It is certain'
-    elif answer_number==2:
-        return 'It is decidedly so'
-    elif answer_number == 3:
-        return 'Yes'
-    elif answer_number == 4:
-        return 'Reply hazy try again'
-    elif answer_number == 5:
-        return 'Ask again later'
-    elif answer_number == 6:
-        return 'Concentrate and ask again'
-    elif answer_number == 7:
-        return 'My reply is no'
-    elif answer_number == 8:
-        return 'Outlook not so good'
-    elif answer_number == 9:
-        return 'Very doubtful'
+logging.basicConfig(filename='registro.log',level=logging.INFO,format='%(asctime)s-%(levelname)s-%(message)s')
 
-print('Ask a yes or no question: ')
-input('>')
-r=random.randint(1,9)
-fortune=gets_answer(r) 
-print(fortune)
+
+respuestas=['It is certain','It is decidedly so','Yes','Reply hazy try again'
+            ,'Ask again later','Concentrate and ask again','My reply is no','Outlook not so good','Very doubtful']
+class Numero_random:
+    def __init__(self,answer_number):
+        self.answer_number=answer_number
+    def gets_answer(self,LISTA):     
+        return LISTA[self.answer_number-1]
+    def aumentar_respuestas(self,LISTA):
+        poner=input('> Ingrese la nueva respuesta ')
+        LISTA+=[poner]
+        return (f'Proceso realizado con exito : {poner}, esta agregado')
+
+
+def jugar(): 
+    n=input('Quieres continuar si o no')
+    while n.upper()=='SI': 
+        r=random.randint(1,len(respuestas))
+        objeto=Numero_random(r)
+        x=input('Quieres ver, aumentar o borrar?')
+        if x.upper()=='VER':
+           fortune=objeto.gets_answer(respuestas)
+           logging.info(f"Tu número fue {r} y la bola dice: {fortune}")
+        elif x.upper()=='AUMENTAR': 
+            aumento=objeto.aumentar_respuestas(respuestas)
+            logging.info(aumento)
+        n=input('Quieres continuar si o no')
+jugar()
